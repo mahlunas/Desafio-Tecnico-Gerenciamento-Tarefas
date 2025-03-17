caa35a37103f0b24398c8e30dbad39bea1153213
Luna <lunasouzamar@gmail.com>
2025-03-17 14:10:19 -0300

    upadte_controller

--- back-end/src/main/java/br/com/marcelleluna/todolist/controller/TodoController.java
+++ back-end/src/main/java/br/com/marcelleluna/todolist/controller/TodoController.java
@@ -7,6 +7,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.marcelleluna.todolist.entity.Todo;
 import br.com.marcelleluna.todolist.sevice.TodoService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
 
 @RestController
 @RequestMapping("/todos")
@@ -18,19 +28,23 @@ public class TodoController {
         this.todoService = todoService;
     }
 
-    List<Todo> create(Todo todo){
+    @PostMapping("path")
+    List<Todo> create(@RequestBody Todo todo){
         return todoService.create(todo);
     }
 
+    @GetMapping("path")
     List<Todo> list(){
         return todoService.list();
     }
 
-    List<Todo> update(Todo todo){
+    @PutMapping
+    List<Todo> update(@RequestBody Todo todo){
         return todoService.update(todo);
     }
     
-    List<Todo> delete(Long id){
+    @DeleteMapping("{id}")
+    List<Todo> delete(@PathVariable("id") Long id){
         return todoService.delete(id);
     }
 }
